4e6947ea97cece261a9fbfb83ba820fc9395ea76
maxminoS <maxminos.1@gmail.com>
2020-12-05 21:05:02 +0700

    Added Evil Nerd Commenter and bind to "M-;"

--- init.el
+++ init.el
@@ -384,6 +384,9 @@
 (use-package company-box
   :hook (company-mode . company-box-mode))
 
+(use-package evil-nerd-commenter
+  :bind (("M-;" . evilnc-comment-or-uncomment-lines)))
+
 (use-package smartparens
   :diminish smartparens-mode
   :config
