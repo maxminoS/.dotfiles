76339bf8778501720302ae2264b63afb8b812e74
maxminoS <maxminos.1@gmail.com>
2020-12-09 15:23:59 +0700

    Added Evil Org

---
+++ lisp/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-org which-key vterm use-package rainbow-delimiters pyvenv python-mode peep-dired org-bullets ivy-rich helpful forge expand-region evil-smartparens evil-nerd-commenter evil-magit evil-collection eshell-toggle doom-themes doom-modeline dired-single dired-sidebar dired-hide-dotfiles dap-mode counsel-projectile company-box benchmark-init all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
