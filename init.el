6811d92fa66368f025c26a1c5b00d8a3853eecc2
maxminoS <maxminos.1@gmail.com>
2020-12-24 14:11:12 +0700

    Fixed indentation at company

--- init.el
+++ init.el
@@ -516,8 +516,8 @@
         (:map lsp-mode-map
          ("<tab>" . company-indent-or-complete-common))
   :custom
-    (company-minimum-prefix-length 1)
-    (company-idle-delay 0.0))
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
 
 (use-package company-box
   :hook (company-mode . company-box-mode))
