e6dab011bc4d328314c39c41d6c38cc45db55115
maxminoS <maxminos.1@gmail.com>
2021-01-24 00:07:25 +0700

    Desktop management with EXWM

--- lisp/custom.el
+++ lisp/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(exwm yaml-mode which-key web-mode vterm visual-fill-column use-package try shackle rjsx-mode rainbow-delimiters pyvenv python-mode peep-dired pdf-tools org-bullets nov lsp-ui json-mode ivy-rich helpful forge expand-region evil-smartparens evil-org evil-nerd-commenter evil-multiedit evil-collection eshell-toggle doom-themes doom-modeline dired-single dired-sidebar dired-open dired-hide-dotfiles define-word dap-mode counsel-projectile company benchmark-init all-the-icons-dired)))
+   '(desktop-environment yaml-mode which-key web-mode vterm visual-fill-column use-package try shackle rjsx-mode rainbow-delimiters pyvenv python-mode peep-dired pdf-tools org-bullets nov lsp-ui json-mode ivy-rich helpful forge exwm expand-region evil-smartparens evil-org evil-nerd-commenter evil-multiedit evil-collection eshell-toggle doom-themes doom-modeline dired-single dired-sidebar dired-open dired-hide-dotfiles define-word dap-mode counsel-projectile company benchmark-init all-the-icons-dired)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
