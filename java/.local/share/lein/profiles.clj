f4fd3ed61325a1f6f7cb8bac62b6009c7f1d4aa3
maxminoS <maxminos.1@gmail.com>
2021-06-27 19:08:08 +0700

    Remove maven from following XDG base directories
    Too much trouble due to the multitude of build tools that depend on
    this, making it extremely messy.

--- java/.local/share/lein/profiles.clj
+++
@@ -1,3 +0,0 @@
-{:user {:local-repo #=(eval (str (System/getenv "XDG_CACHE_HOME") "/m2/repository"))
-        :repositories  {"local" {:url #=(eval (str "file://" (System/getenv "XDG_CACHE_HOME") "/m2/repository"))
-                                 :releases {:checksum :ignore}}}}}
